5e53d17f10515f0e7c52e48ea3feab193f3e95a3
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 21:00:18 +0000

    Sun May 01 21:00:18 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,10 +6,13 @@ def main():
     hello('John')
 
 def hello(friend_name):
-    if friend_name == "":
-        return 'Hello World'
-    else:
-        return 'Hello ' + friend_name
+    # if friend_name == "":
+    #     return 'Hello World'
+    # else:
+    #     return 'Hello ' + friend_name
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     main()
+
+# print(hello('Ted'))
--- lib/solutions/HLO/test_hello_solution.py
+++ lib/solutions/HLO/test_hello_solution.py
@@ -7,8 +7,11 @@ class TestSolution(unittest.TestCase):
     def test_hello(self):
         s1='''Hello John'''
         s2='''Hello World'''
-        self.assertEqual(hello('John'), s1)
-        self.assertEqual(hello(''), s2)
+        s3='''Hello, World!'''
+        # self.assertEqual(hello('John'), s1)
+        # self.assertEqual(hello(''), s2)
+        self.assertEqual(hello('Ted'), s3)
+        self.assertEqual(hello(''), s3)
 
 if __name__ == '__main__':
     unittest.main()
