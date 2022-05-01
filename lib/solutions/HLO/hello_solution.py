b14d4a93319522c9a125f7bd31f402c6c3f46949
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 20:54:31 +0000

    Sun May 01 20:54:31 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,11 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
+def main():
+    hello('John')
+
 def hello(friend_name):
-    raise NotImplementedError()
+    return 'Hello ' + 
+
+if __name__ == "__main__":
+    main()
--- lib/solutions/HLO/test_hello_solution.py
+++ lib/solutions/HLO/test_hello_solution.py
@@ -1,13 +1,12 @@
 import unittest
-from sum_solution import *
+from hello_solution import *
 
 
 
 class TestSolution(unittest.TestCase):
-    def test_compute(self):
-        self.assertEqual(compute(1,2), 3)
-        self.assertEqual(compute(2,3), 5)
-        self.assertEqual(compute(0,100), 100)
+    def test_hello(self):
+        s1='''Hello John'''
+        self.assertEqual(hello('John'), s1)
 
 if __name__ == '__main__':
     unittest.main()
