08c151f231c67c0ae91676c0c0eee4f0e7b9ebb0
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 21:38:36 +0000

    Sun May 01 21:38:36 UTC 2022

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -9,10 +9,17 @@ class TestSolution(unittest.TestCase):
         sku2 = '''abcd2'''
         sku3 = ''''''
         sku4 = '''aababd'''
+        sku5 = '''zmjsd'''
+        sku6 = '''aababdaaaaaac'''
+        sku7 = '''AA'''
         self.assertEqual(checkout(sku1), 115)
         self.assertEqual(checkout(sku2), -1)
         self.assertEqual(checkout(sku3), -1)
-        self.assertEqual(checkout(sku4), -1)
+        self.assertEqual(checkout(sku4), 190)
+        self.assertEqual(checkout(sku5), -1)
+        self.assertEqual(checkout(sku6), 470)
+        self.assertEqual(checkout(sku7), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
