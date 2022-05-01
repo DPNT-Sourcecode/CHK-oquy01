9b2fabea359dcb0b876b5f8a51cd4976d154a11a
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 21:14:36 +0000

    Sun May 01 21:14:36 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,11 @@
 def checkout(skus):
     #create array for prices
     #convert all strings tolower for consistency
-    #assuming 
+    #assuming input is a string of letters, without spaces or commas
+    #if the counter dict reaches 3 items, take a discount off the total prices, and reset the counter for that letter
+
+    total = 0
+    prices = {a:}
 
     raise NotImplementedError()
+
