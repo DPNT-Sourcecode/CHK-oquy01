2767583723bd43a03fa27223ae1f8dc9702362af
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 23:44:14 +0000

    Sun May 01 23:44:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -74,6 +74,42 @@ def checkout(skus):
     counter = Counter(skus)
     newCounter = updateCounterDict(counter)
 
+
+    '''+------+-------+----------------+
+round4
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+
+
+'''
+
     for key,value in newCounter.items():
         if key == 'A':
             total += totalValueOfAs(newCounter[key])
@@ -82,7 +118,19 @@ def checkout(skus):
         elif key == 'H':
             #5H for 45, 10H for 80 
             tens = newCounter[key] // 10
-            remTens = 
+            remTens = newCounter[key] % 10
+            fives = remTens // 5
+            remainder = remTens % 5
+            return (tens * 80) + (fives * 45) + (remainder * prices[key])
+        elif key == 'K':
+            #5H for 45, 10H for 80 
+            tens = newCounter[key] // 10
+            remTens = newCounter[key] % 10
+            fives = remTens // 5
+            remainder = remTens % 5
+
+            return (tens * 80) + (fives * 45) + (remainder * prices[key])
+
         if key == 'N':
             counterDict['M'] = max(counterDict['M'] - counterDict['N'] // 3,0)
         if key == 'R':
@@ -149,3 +197,4 @@ def updateCounterDict(counterDict):
 
 if __name__ == "__main__":
     main()
+
