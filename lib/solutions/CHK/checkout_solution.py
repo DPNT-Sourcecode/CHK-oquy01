084f29798197e61a12c1e0552706b8c714b7d3fa
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 21:11:36 +0000

    Sun May 01 21:11:36 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,8 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    #create array for prices
+    #convert all strings tolower for consistency
+    #assuming 
+
     raise NotImplementedError()
--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -5,7 +5,8 @@ from checkout_solution import *
 
 class TestSolution(unittest.TestCase):
     def test_checkout(self):
-        sku1 = ''''''
+        sku1 = '''abcd'''
+        self.assertEqual(checkout(sku1), 115)
 
 if __name__ == '__main__':
     unittest.main()
