1c30013e362097a1b2a1227dd9c55c8f422f5c06
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 21:08:36 +0000

    Sun May 01 21:08:36 UTC 2022

---
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -0,0 +1,11 @@
+import unittest
+from checkout_solution import *
+
+
+
+class TestSolution(unittest.TestCase):
+    def test_checkout(self):
+        sku1 = ''''''
+
+if __name__ == '__main__':
+    unittest.main()
