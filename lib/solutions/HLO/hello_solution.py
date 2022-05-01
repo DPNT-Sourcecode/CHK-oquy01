4429721447e15f8a751421ca62e8ea78e2eebd9c
sbx_user1051 <sbx_user1051@169.254.1.1>
2022-05-01 21:03:17 +0000

    Sun May 01 21:03:17 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,13 +6,14 @@ def main():
     hello('John')
 
 def hello(friend_name):
-    # if friend_name == "":
-    #     return 'Hello World'
-    # else:
-    #     return 'Hello ' + friend_name
+    if friend_name == "":
+        return 'Hello, World!'
+    else:
+        return 'Hello ' + friend_name
     return 'Hello, World!'
 
 if __name__ == "__main__":
     main()
 
 # print(hello('Ted'))
+
